8781b758fb9d20434cb9858bbb00fdcf67678bdb
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2023-12-08 14:47:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let hexcode="#"
+	let n=shortHex.length;
+    for (let i = 0; i < n; i++) {
+   if(shortHex[i]!="#"){
+  //console.log(shortHex[i]);
+        hexcode+=shortHex[i]+shortHex[i];
+        //console.log(hexcode)
+        }
+}
+    return hexcode;
+	
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+// const shortHex = prompt("Enter Short Hex.");
+// alert(extendHex(shortHex));
